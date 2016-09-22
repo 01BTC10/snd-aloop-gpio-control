722b0a67243033818deec2b7cfa96e2453245136
r2d2 <tayaoutlk@gmail.com>
2016-09-22 20:55:14 +0200

    Update mysnd_aloop.c

--- mysnd_aloop.c
+++ mysnd_aloop.c
@@ -13,6 +13,9 @@
  *  A next major update in 2010 (separate timers for playback and capture):
  *  Copyright (c) Jaroslav Kysela <perex@perex.cz>
  *
+ *  Added option to control GPIO pin when playback is active in 2016:
+ *  Copyright (c) Tayaout Kuberek <tayaoutlk@gmail.com>
+ *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation; either version 2 of the License, or
